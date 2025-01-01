62459bfc23c1e48a13e6c696052f255f85c921a9
Your Name <you@example.com>
2025-01-01 10:47:54 +0000

    ..

--- test_python.py
+++ test_python.py
@@ -1,11 +1,22 @@
 import redis
 import os
 import json
+import requests
 
-# Connect to Redis
+
+# def get_redis_url():
+#     headers = {
+#         'Authorization': 'Bearer 2r1YIzNo0KpPmyZSpKQFhcDtrpO_7SCPzbMxakXt53jryGikf',
+#         'Ngrok-Version': '2'
+#     }
+#     response = requests.get('https://api.ngrok.com/endpoints', headers=headers)
+#     return response.json()["endpoints"][0]["public_url"]
+# # Connect to Redis
+# r = redis.from_url(get_redis_url(),
+#                    )
 r = redis.Redis(
-    host='ohadrubin.com',
-    port=39912,
+    host="4.tcp.eu.ngrok.io",
+    port=12954,
     # ohadrubin.com:
     password=os.getenv('REDIS_PASSWORD'),
     decode_responses=True
