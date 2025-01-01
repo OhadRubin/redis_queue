77bb240b7b9dc78d213ccf29c888fec1d5765209
Your Name <you@example.com>
2025-01-01 09:14:51 +0000

    ..

--- start_tunnel.sh
+++ start_tunnel.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
+
 # it was obtained from:
 # wget https://github.com/amalshaji/portr/releases/download/0.0.26-beta/portr_0.0.26-beta_Linux_x86_64.zip
 # unzip portr_0.0.26-beta_Linux_x86_64.zip 
 ./portr auth set --token $PORTR_KEY --remote $GATEWAY_DOMAIN
-
 
 while true; do
     ./portr tcp 6379 -s redis
