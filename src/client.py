b9e85087a0a1819ad97bb70f6da68d7cf6cc06f5
Your Name <you@example.com>
2025-01-24 05:50:40 +0000

    ..

--- src/client.py
+++ src/client.py
@@ -3,24 +3,31 @@ import os
 import json
 import requests
 
-NGROK_API_KEY = os.getenv('NGROK_API_KEY')
 
-def get_client(**kwargs):
+
+def get_redis_url():
     headers = {
-        'Authorization': f'Bearer {NGROK_API_KEY}',
+        'Authorization': f'Bearer {os.getenv("NGROK_API_KEY")}',
         'Ngrok-Version': '2'
     }
     
     response = requests.get('https://api.ngrok.com/endpoints', headers=headers)
     ngrok_url = response.json()["endpoints"][0]["public_url"]
     addr, port = ngrok_url.replace("tcp://","").split(":")
-    r = redis.Redis(
-        host=addr,
-        port=int(port),
-        password=os.getenv('REDIS_PASSWORD'),
+    password = os.getenv('REDIS_PASSWORD')
+    return f"redis://:{password}@{addr}:{port}"
+
+
+def get_client(**kwargs):
+    url = get_redis_url()
+    # host=addr,
+    # port=int(port),
+    # password=os.getenv('REDIS_PASSWORD'),
+    r = redis.Redis.from_url(url,
         **kwargs
         # decode_responses=True
     )
+    # redis.Redis.from_url(f"redis://{addr}:{port}", password=os.getenv('REDIS_PASSWORD'), **kwargs)
     return r
 
 if __name__ == "__main__":
